ac0adad85be568fce737955aad2565c145196614
Mirt12 <Mirt12@yandex.ru>
2023-06-27 14:59:27 +0200

    add test: OpenHomePageTest to HomePageRestTests class

--- src/test/java/by/itacademy/tatjana/balashevich/api/HomePageRestTests.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/HomePageRestTests.java
@@ -7,20 +7,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static io.restassured.RestAssured.given;
+
 public class HomePageRestTests {
+    WebDriver driver = new ChromeDriver();
+
     @Test
-    public void openBrowserTest() throws InterruptedException {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("--disable-cache");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get("https://www.meetup.com/");
-        Thread.sleep(2000);
-        String homePageAcceptCookiesBtnLocator = "//button[@id='onetrust-accept-btn-handler']";
-        WebElement submitCookiesBtn = driver.findElement(By.xpath(homePageAcceptCookiesBtnLocator));
-        submitCookiesBtn.click();
-        //to check GET response
+    public void OpenHomePageTest() throws InterruptedException {
+        String url = "https://www.meetup.com";
+                given().
+                when().get(url).
+                then().assertThat().
+                statusCode(200).log().body();
         driver.quit();
     }
 }
