d70c09f602d754cc4e1fb92757012707621e3882
Mirt12 <Mirt12@yandex.ru>
2023-06-27 15:45:32 +0200

    add temp tests for REST

--- src/test/java/by/itacademy/tatjana/balashevich/api/HomePageRestTests.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/HomePageRestTests.java
@@ -1,11 +1,8 @@
 package by.itacademy.tatjana.balashevich.api;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static io.restassured.RestAssured.given;
 
@@ -13,12 +10,43 @@ public class HomePageRestTests {
     WebDriver driver = new ChromeDriver();
 
     @Test
-    public void OpenHomePageTest() throws InterruptedException {
+    public void OpenHomePageTest() {
         String url = "https://www.meetup.com";
-                given().
+        given().
                 when().get(url).
                 then().assertThat().
                 statusCode(200).log().body();
         driver.quit();
     }
+
+    @Test
+    public void OpenLoginFormTest() {
+        //how to open login Form?
+        String url = "https://www.meetup.com";
+        given().
+                when().get(url).
+                then().assertThat().
+                statusCode(200).log().body();
+        driver.quit();
+    }
+
+    @Test
+    public void SubmitLoginFormTest() {
+        String url = "https://www.meetup.com/gql";
+        String body = "{\n" +
+                "    \"operationName\": \"requireCaptcha\",\n" +
+                "    \"variables\": {\n" +
+                "        \"captchaType\": \"LOGIN\"\n" +
+                "    },\n" +
+                "    \"query\": \"query requireCaptcha($captchaType: CaptchaType) {\\n  requireCaptcha(captchaType: $captchaType) {\\n    requireCaptcha\\n    error {\\n      ...Error\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment Error on PayloadError {\\n  code\\n  message\\n  field\\n  __typename\\n}\\n\"\n" +
+                "}";
+        given().header("Content-Type", "application/json").body(body).
+                when().post(url).
+                then().assertThat().
+                statusCode(200).log().body();
+        driver.quit();
+    }
+
+
+
 }
