4f81f3539f5f28706e22ef9bd2852437c8915a91
Mirt12 <Mirt12@yandex.ru>
2023-06-28 19:06:12 +0200

    add SubmitLoginFormTest

--- src/test/java/by/itacademy/tatjana/balashevich/api/YvesRocherLoginRestTests.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/YvesRocherLoginRestTests.java
@@ -1,9 +1,22 @@
 package by.itacademy.tatjana.balashevich.api;
 
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 
 public class YvesRocherLoginRestTests {
+
     @Test
     public void SubmitLoginFormTest() {
+        String url = "https://api.y-r.by/api/v1/token";
+        String body = "{\n" +
+                "    \"email\": \"tbalashevich@bk.ru\",\n" +
+                "    \"password\": \"PostinG@2579!\",\n" +
+                "    \"remember\": true\n" +
+                "}";
+        given().header("Content-Type", "application/json").body(body).
+                when().post(url).
+                then().assertThat().
+                statusCode(200).log().body();
+    }
 }
