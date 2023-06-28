8b13a7be42bdf63faa62199311420f85728a491e
Mirt12 <Mirt12@yandex.ru>
2023-06-28 22:24:48 +0200

    add validUserCanLoginTest, invalidUserSeesErrorWhenLoginTest to Yves RocherLoginTests class

--- src/test/java/by/itacademy/tatjana/balashevich/api/YvesRocherLoginRestTests.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/YvesRocherLoginRestTests.java
@@ -3,20 +3,44 @@ package by.itacademy.tatjana.balashevich.api;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 public class YvesRocherLoginRestTests {
 
     @Test
-    public void SubmitLoginFormTest() {
+    public void validUserCanLoginTest() {
         String url = "https://api.y-r.by/api/v1/token";
         String body = "{\n" +
                 "    \"email\": \"tbalashevich@bk.ru\",\n" +
                 "    \"password\": \"PostinG@2579!\",\n" +
                 "    \"remember\": true\n" +
                 "}";
-        given().header("Content-Type", "application/json").body(body).
+        given().body(body)
+                .header("Content-Type", "application/json")
+                .header("accept", "application/json").
                 when().post(url).
-                then().assertThat().
-                statusCode(200).log().body();
+                then().log().body().
+                assertThat().statusCode(200).
+                assertThat().body(containsString("token")).
+                assertThat().body(not(containsString("message")));
+    }
+
+    @Test
+    public void invalidUserSeesErrorWhenLoginTest() {
+        String url = "https://api.y-r.by/api/v1/token";
+        String body = "{\n" +
+                "    \"email\": \"tbalashevichTEST@bk.ru\",\n" +
+                "    \"password\": \"PostinG@2579TEST!\",\n" +
+                "    \"remember\": true\n" +
+                "}";
+        given().body(body)
+                .header("Content-Type", "application/json")
+                .header("accept", "application/json").
+                when().post(url).
+                then().log().body().
+                assertThat().statusCode(401).
+                assertThat().body(containsString("message")).
+                assertThat().body(not(containsString("token")));
     }
 }
