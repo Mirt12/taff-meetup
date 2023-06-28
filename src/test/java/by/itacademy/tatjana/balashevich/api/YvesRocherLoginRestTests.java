526a828888455dc8f61d8646dff4110c1158d199
Mirt12 <Mirt12@yandex.ru>
2023-06-28 19:02:17 +0200

    add YvesRocherLoginRestTests class

---
+++ src/test/java/by/itacademy/tatjana/balashevich/api/YvesRocherLoginRestTests.java
@@ -0,0 +1,9 @@
+package by.itacademy.tatjana.balashevich.api;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+public class YvesRocherLoginRestTests {
+    @Test
+    public void SubmitLoginFormTest() {
+}
